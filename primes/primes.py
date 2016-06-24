fcccf8b16a021247bd3c28ccaad8552cdb7f8063
Allison Lyon <alyon@Allison-Lyons-Computer.local>
2016-06-24 16:24:30 -0700

    primes

--- primes/primes.py
+++ primes/primes.py
@@ -13,11 +13,40 @@ For example::
 
 """
 
+def is_prime(num):
+    """Checks if num is prime"""
+
+    if num < 2:
+        return False
+    if num == 2:
+        return True
+    if num % 2 == 0:
+        return False
+
+    factor = 3
+
+    while factor ** 2 <= num:
+        if num % factor == 0:
+            return False
+        factor += 2
+
+    return True
+
 
 def primes(count):
     """Return count number of prime numbers, starting at 2."""
 
-    
+    primes = []
+
+    num = 2
+
+    while len(primes) < count:
+        if is_prime(num):
+            primes.append(num)
+        num += 1
+
+    return primes
+
 
 if __name__ == '__main__':
     import doctest
