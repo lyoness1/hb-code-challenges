e75ee556219e0bd1afb0e771f0776c98af389710
lyoness1 <alyoness1@gmail.com>
2016-06-29 17:05:00 -0700

    updated

--- rev-linked-list/reversell.py
+++ rev-linked-list/reversell.py
@@ -25,14 +25,14 @@ class Node(object):
         '321'
         """
 
-        out = []
+        output = []
         n = self
 
         while n:
             out.append(str(n.data))
             n = n.next
 
-        return "".join(out)
+        return "".join(output)
 
 
 def reverse_linked_list(head):
