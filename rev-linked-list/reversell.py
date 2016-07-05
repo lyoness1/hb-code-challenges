c0aac2da207543b107389072c842c2c43e739d6e
Allison Lyon <alyoness1@gmail.com>
2016-07-05 08:55:41 -0700

    reverse ll recursive and iterative

--- rev-linked-list/reversell.py
+++ rev-linked-list/reversell.py
@@ -3,7 +3,9 @@
 For example:
 
     >>> ll = Node(1, Node(2, Node(3)))
-    >>> reverse_linked_list(ll).as_string()
+    >>> reverse_linked_list_iteratively(ll).as_string()
+    '321'
+    >>> reverse_linked_list_recursively(ll).as_string()
     '321'
 """
 
@@ -35,22 +37,46 @@ class Node(object):
         return "".join(out)
 
 
-def reverse_linked_list(head):
+def reverse_linked_list_iteratively(head):
     """Given LL head node, return head node of new, reversed linked list.
 
     >>> ll = Node(1, Node(2, Node(3)))
-    >>> reverse_linked_list(ll).as_string()
+    >>> reverse_linked_list_iteratively(ll).as_string()
     '321'
     """
 
-    def recurse_through_ll(node, nodestack):
+    output_head = None
+    current = head
+
+    while current:
+        output_head = Node(current.data, output_head)
+        current = current.next
+
+    return output_head
+
+
+def reverse_linked_list_recursively(head):
+    """Given LL head node, return head node of new, reversed linked list.
+
+    >>> ll = Node(1, Node(2, Node(3)))
+    >>> reverse_linked_list_recursively(ll).as_string()
+    '321'
+    """
+
+    tail = Node(head.data)
+
+    def recurse_through_ll_helper(node, next):
         """progresses to end of ll"""
 
         # base case: reached tail
-        if head.next == None:
-            return 
+        if node.next == None:
+            return Node(node.data, next)
 
-        # progress toward base case: 
+        # progress toward base case:
+        return recurse_through_ll_helper(node.next, Node(node.data, next))
+
+
+    return recurse_through_ll_helper(head.next, tail)
 
 
 if __name__ == '__main__':
