eb86f0debc23603542c74fe6ab5c2170789b0814
Allison Lyon <alyoness1@gmail.com>
2016-10-21 20:14:19 -0700

    takeaway

--- medium/takeaway/takeaway.py
+++ medium/takeaway/takeaway.py
@@ -36,7 +36,28 @@
 
 
 def takeaway(n):
-    """Determine which play can win takeaway."""
+    """Determine which play can win takeaway. 2, 3, 5"""
+
+    # memoize whether current player can win at n stones
+    # include index n
+    memo = [False for num in xrange(n+1)]
+    
+    # fill in memo for each play
+    # i = 0 or 1 will always be losses for current player
+    for i in xrange(2, n+1):
+        # try every option
+        for option in [2, 3, 5]:
+            # if current play returns False, other player can't win
+            # if other player can't win, current player will win
+            if i >= option and memo[i - option] == False:
+                memo[i] = True
+                break
+
+    # add one for zero indexed list
+    return 1 if memo[n] == True else 2
+
+
+
 
 if __name__ == '__main__':
     import doctest
